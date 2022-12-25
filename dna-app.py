558b6f8eac2b4db30a652cb0d29ee44795377833
Vinicius999 <viniciussilva.dsc@gmail.com>
2022-12-25 14:48:03 -0300

    Add app information

--- dna-app.py
+++ dna-app.py
@@ -7,6 +7,9 @@ image = Image.open('images/dna-logo.png')
 
 st.image(image, use_column_width = True)
 
+#######################
+# Page Title
+#######################
 st.write('''
 # DNA Nucleotide Count Web App
 
@@ -14,3 +17,23 @@ This app counts the nucleotide composition of query DNA!
 
 ---
 ''')
+
+
+#######################
+# Input Text Box
+#######################
+st.header('Enter DNA sequence')
+
+sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
+
+sequence = st.text_area('Sequence input', sequence_input, height=150)
+sequence = sequence.splitlines()
+sequence = sequence[1:] # Skip the sequence name (first line)
+sequence = ''.join(sequence) # Concatenate list to string
+
+st.write('''
+---
+''')
+
+st.header('INPUT (DNA Query)')
+sequence
