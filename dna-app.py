fd0f9f13bb8fa972975fe31cf62c1723e89d4ea7
Vinicius999 <viniciussilva.dsc@gmail.com>
2022-12-26 20:40:55 -0300

    Update files

--- dna-app.py
+++ dna-app.py
@@ -37,3 +37,24 @@ st.write('''
 
 st.header('INPUT (DNA Query)')
 sequence
+
+## DNA nucleotide count
+st.header('OUTPUT (Nucleotide Count)')
+
+def DNA_nucleotide_count(seq):
+    d = dict([
+        ('A', seq.count('A')),
+        ('T', seq.count('T')),
+        ('G', seq.count('G')),
+        ('C', seq.count('C'))
+    ])
+    
+    return d
+
+X = DNA_nucleotide_count(sequence)
+
+X_label = list(X)
+X_values = list(X.values())
+
+X
+
