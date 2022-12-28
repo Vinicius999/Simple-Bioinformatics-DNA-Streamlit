c3c419ef9f29f1b51b5bbf4c4b4da7ce4a0891f4
Vinicius999 <viniciussilva.dsc@gmail.com>
2022-12-27 23:53:45 -0300

    Showing results

--- dna-app.py
+++ dna-app.py
@@ -51,10 +51,29 @@ def DNA_nucleotide_count(seq):
     
     return d
 
-X = DNA_nucleotide_count(sequence)
+### Ways to show the results
 
-X_label = list(X)
-X_values = list(X.values())
+# 1. Print dictionary
+X = DNA_nucleotide_count(sequence)
+X_label, X_values = list(X), list(X.values())
 
 X
+
+# 2. Print text
+st.subheader('2. Print text')
+st.text(f'There are {str(X["A"])} adenine (A)')
+st.text(f'There are {str(X["T"])} adenine (T)')
+st.text(f'There are {str(X["G"])} adenine (G)')
+st.text(f'There are {str(X["C"])} adenine (C)')
+
+# 3. Display DataFrame
+st.subheader('3. Display DataFrame')
+df = pd.DataFrame.from_dict(X, orient='index')
+df = df.rename({0: 'count'}, axis='columns')
+df.reset_index(inplace=True)
+df = df.rename(columns = {'index': 'nucleotide'})
+
+st.write(df)
+
+# 4. Display Bar Chart using Altair
 
